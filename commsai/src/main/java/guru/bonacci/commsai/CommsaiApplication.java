f32d70d0978f346ebfb8089be08b60b5d2f83480
Jeffrey van Helden <jeffrey.van.helden@nzta.govt.nz>
2026-01-01 07:20:50 +1300

    first commit

---
+++ commsai/src/main/java/guru/bonacci/commsai/CommsaiApplication.java
@@ -0,0 +1,13 @@
+package guru.bonacci.commsai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommsaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommsaiApplication.class, args);
+	}
+
+}
---
+++ commsai/src/test/java/guru/bonacci/commsai/CommsaiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.bonacci.commsai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommsaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
